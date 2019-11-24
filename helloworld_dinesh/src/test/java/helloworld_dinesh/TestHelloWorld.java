95cc0a9fd6de72b6a782031f20a3deb5ba3484d2
Dines <Dines@DineshPC>
2019-11-24 16:06:22 +0530

    first commit

---
+++ helloworld_dinesh/src/test/java/helloworld_dinesh/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld_dinesh;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+
+	public void testHelloWorld()
+	{
+	
+		System.out.println("Hello World!!!");
+	}
+	
+}
